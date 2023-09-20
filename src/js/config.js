f3011094aec048d9dd2c9b31a877bc4d0cbdd98a
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-09-20 15:25:44 -0300

    Uploading a new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '9a084dc6-3e3c-4ae3-b1a7-1e7acac79ca6';
+export const MODAL_CLOSE_SEC = 2.5;
